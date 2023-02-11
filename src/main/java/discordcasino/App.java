3191f52240475549d754a6006fde47d94e059193
Brett-Ruane <bgr28288@uga.edu>
2023-02-11 18:48:12 -0500

    set discord bot up

---
+++ src/main/java/discordcasino/App.java
@@ -0,0 +1,31 @@
+package discordcasino;
+
+import java.util.Collections;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.interaction.command.*;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+
+public class App extends ListenerAdapter {
+    public static void main(String[] args) {
+        JDA casino = JDABuilder
+                .createLight("MTA3MzUyNTAyMjEwNDIyNzg2MA.GTnhor.1JHNIcr_7AZUKXo4_6AhQppZPEuVK2YMMAU7IM",
+                        Collections.emptyList())
+                .addEventListeners(new App())
+                .setActivity(Activity.playing("Type /ping"))
+                .build();
+        casino.updateCommands().addCommands(Commands.slash("blackjack", "Plays BlackJack")).queue();
+    }
+
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        // make sure we handle the right command
+        switch (event.getName()) {
+            case "blackjack":
+
+                break;
+        }
+    }
+}
